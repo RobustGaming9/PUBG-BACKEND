495ded7e71e54c9d673d3d7994682bc2d8b2410d
KrishRamani2 <krishramani02@gmail.com>
2025-04-20 21:42:32 +0530

    Update

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/user",userRoute);
 app.use('/api/bill', billRoutes);
-
+app.use(cors({
+    origin: ['https://bill-generator-backend.vercel.app/'],
+    credentials: true,
+  }));
 app.listen(port,()=>{
     console.log("Backend is running on port:"+port);
 })
