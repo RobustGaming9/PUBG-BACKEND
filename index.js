687782abb48e1b1b2e7ba151d2abe2dd6517fef5
RobustGaming9 <robustgaming972@gmail.com>
2025-06-21 11:28:03 +0530

    update

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ require('./database/db');
 
 const express = require("express");
 const app = express();
-const  cors = require("cors");
+const cors = require("cors");
 const tournamentRoutes = require('./routes/Tournaments');
 
-app.use(express.json());
-app.use('/api/tournaments', tournamentRoutes);
-app.use(cors());
 app.use(cors({
-    origin: ['https://leaderboard-psi-seven.vercel.app/'],
-    credentials: true,
-  }));
+  origin: 'https://leaderboard-psi-seven.vercel.app',
+  credentials: true
+}));
 
-app.listen(5050,()=>{
-    console.log("Backend is running on port:"+5050);
-})
+
+app.use(express.json());
+
+
+app.use('/api/tournaments', tournamentRoutes);
+
+const port = process.env.PORT || 5050;
+app.listen(port, () => {
+  console.log(`Backend is running on port: ${port}`);
+});
