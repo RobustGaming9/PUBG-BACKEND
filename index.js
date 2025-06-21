390f7985d798eed30664f92dc3b47eef1c30203a
RobustGaming9 <robustgaming972@gmail.com>
2025-06-21 11:30:54 +0530

    update

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ require('dotenv').config();
 require('./database/db');
 
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const tournamentRoutes = require('./routes/Tournaments');
 
+const app = express();
+
 app.use(cors({
   origin: 'https://leaderboard-psi-seven.vercel.app',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
 }));
 
+app.options('*', cors());
 
 app.use(express.json());
-
-
 app.use('/api/tournaments', tournamentRoutes);
 
 const port = process.env.PORT || 5050;
